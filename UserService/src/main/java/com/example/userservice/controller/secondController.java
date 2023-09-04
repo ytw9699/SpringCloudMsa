73c038e04d3fccbd3509503dd27330e802d1e9e2
taewon <ytw9699@buyaladdinrnd.com>
2023-09-04 21:54:48 +0900

    feat : Spring Cloud Gateway - Filter 적용

---
+++ UserService/src/main/java/com/example/userservice/controller/secondController.java
@@ -0,0 +1,25 @@
+package com.example.userservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/second-service")
+public class secondController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        log.info("second service welcome");
+        return "second service welcome";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header){
+        log.info(header);
+        return "message="+header;
+    }
+}
