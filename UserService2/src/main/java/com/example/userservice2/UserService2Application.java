989ca99b38b4c6d7e129a03f99e696e7c11675c8
home <ytw9699>
2023-09-08 23:00:03 +0900

    feat : userservice2 플젝 생성

---
+++ UserService2/src/main/java/com/example/userservice2/UserService2Application.java
@@ -0,0 +1,15 @@
+package com.example.userservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserService2Application.class, args);
+    }
+
+}
