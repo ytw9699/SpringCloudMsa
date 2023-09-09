764344ee49d3c9de2c2d6505f9e6ce45bbfc2080
home <ytw9699>
2023-09-09 12:29:34 +0900

    feat : jpa, h2 연동 및 회원가입 api

---
+++ UserService2/src/main/java/com/example/userservice2/vo/ResponseUser.java
@@ -0,0 +1,10 @@
+package com.example.userservice2.vo;
+
+import lombok.Data;
+
+@Data
+public class ResponseUser {
+    private String email;
+    private String name;
+    private String userId;
+}
