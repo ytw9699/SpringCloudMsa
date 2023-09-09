ed0f9ff6bba9918e72d43647373f4002668fee60
home <ytw9699>
2023-09-09 23:19:07 +0900

    feat : 시큐리티 연동

--- UserService2/src/main/java/com/example/userservice2/UserService2Application.java
+++ UserService2/src/main/java/com/example/userservice2/UserService2Application.java
@@ -3,6 +3,8 @@ package com.example.userservice2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +12,11 @@ public class UserService2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(UserService2Application.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
