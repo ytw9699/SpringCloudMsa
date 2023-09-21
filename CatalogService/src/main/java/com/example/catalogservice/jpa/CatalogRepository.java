0c1e987a1d2d211a7997dc517fb58c055467f48a
taewon <ytw9699@buyaladdinrnd.com>
2023-09-21 22:00:00 +0900

    feat : 카탈로그서비스 생성

---
+++ CatalogService/src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,6 @@
+package com.example.catalogservice.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CatalogRepository extends JpaRepository<CatalogEntity, Long> {
+}
