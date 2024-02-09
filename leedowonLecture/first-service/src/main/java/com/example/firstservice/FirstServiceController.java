ec56ed8353b0f85f9c4a4f9bd8dde97b3493204a
TaeWon <ytw9699@naver.com>
2024-02-10 01:38:53 +0900

    feat : first-service 생성

---
+++ leedowonLecture/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,18 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class FirstServiceController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        log.info("Welcome to the First service.");
+        return "Welcome to the First service.";
+    }
+}
