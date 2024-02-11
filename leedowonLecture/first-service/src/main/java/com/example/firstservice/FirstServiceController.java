fb333363734cea3750486edf2c82755ff6236d2e
TaeWon <ytw9699@naver.com>
2024-02-11 16:54:07 +0900

    fix : Spring Cloud Gateway - Filter 적용

--- leedowonLecture/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ leedowonLecture/first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -2,6 +2,7 @@ package com.example.firstservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,11 @@ public class FirstServiceController {
     public String welcome() {
         log.info("Welcome to the First service.");
         return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello world in first service";
     }
 }
