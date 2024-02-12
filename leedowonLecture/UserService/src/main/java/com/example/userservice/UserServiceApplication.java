559bc1f966b5b90d6139d43c657357bcffb21745
TaeWon <ytw9699@naver.com>
2024-02-12 20:06:24 +0900

    fix : Users Microservice - welcome() 메소드

--- leedowonLecture/UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ leedowonLecture/UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient// 유레카 서버에 등록
 //@EnableEurekaClient 이게 EnableDiscoveryClient를 구현해서 더 상품화 시켜둔것이긴하다. 어떤걸 사용해도됨
 public class UserServiceApplication {
 
