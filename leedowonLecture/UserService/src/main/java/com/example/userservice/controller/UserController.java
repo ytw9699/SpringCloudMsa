0c5fc7adf8632cf727031b0fe94ee8d7708ed47e
TaeWon <ytw9699@naver.com>
2024-02-13 00:09:57 +0900

    fix : Users Microservice - JPA 1

--- leedowonLecture/UserService/src/main/java/com/example/userservice/controller/UserController.java
+++ leedowonLecture/UserService/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,23 +1,30 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
 
     private Environment env;
+    private final UserService userService;
 
     @Autowired
     private Greeting greeting;
 
     @Autowired
-    public UserController(Environment env){
+    public UserController(Environment env,  UserService userService){
+        this.userService = userService;
         this.env = env;
     }
 
@@ -30,5 +37,19 @@ public class UserController {
     public String welcome(){
         //return env.getProperty("greeting.message");
         return greeting.getMessage();
+    }
+
+    /** 회원 가입 */
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+
+        userService.createUser(userDto);
+
+        return "createUser";
     }
 }
---
+++ leedowonLecture/UserService/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.userservice.service;
+
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.jpa.UserEntity;
+import com.example.userservice.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);//변환
+
+        userEntity.setEncryptedPwd("encrypted_password");
+
+        userRepository.save(userEntity);
+
+        return null;
+    }
+}
