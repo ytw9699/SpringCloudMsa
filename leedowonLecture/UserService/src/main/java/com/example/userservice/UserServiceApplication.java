af9c226ba799177322ee43bc90ef90b286d29d1c
TaeWon <ytw9699@naver.com>
2024-02-14 00:20:14 +0900

    fix : Users Microservice - Spring Security 연동

--- leedowonLecture/UserService/src/main/java/com/example/userservice/UserServiceApplication.java
+++ leedowonLecture/UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient// 유레카 서버에 등록
@@ -14,4 +16,9 @@ public class UserServiceApplication {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    //사용자가 입력한 비밀번호 암호화 변환 작업
 }
---
+++ leedowonLecture/UserService/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -0,0 +1,19 @@
+package com.example.userservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration//우선순위 앞서서 빈등록함
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override    // 권한 관련 로직 처리
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+
+        http.headers().frameOptions().disable();
+    }
+}
