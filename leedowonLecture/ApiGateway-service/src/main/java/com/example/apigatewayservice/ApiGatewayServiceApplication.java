139618eb57230c25b145b0fb11477699092f2f23
TaeWon <ytw9699@naver.com>
2024-02-19 11:02:20 +0900

    fix : Spring Cloud Gateway에서 Spring Cloud Config 연동2

--- leedowonLecture/ApiGateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
+++ leedowonLecture/ApiGateway-service/src/main/java/com/example/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,7 +1,10 @@
 package com.example.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
@@ -10,4 +13,10 @@ public class ApiGatewayServiceApplication {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public HttpTraceRepository httpTraceRepository(){
+        return new InMemoryHttpTraceRepository();
+        //클라이언트가 요청했던 정보가 메모리에 담겨서 엔드포인트로 해당 값 사용가능
+    }
 }
+
